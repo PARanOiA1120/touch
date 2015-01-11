a0d90df051779e5683efb53a4900c5363c8b1498
paranoia1120 <yuexin@umail.ucsb.edu>
2015-01-10 19:44:22 -0800

    project setup

---
+++ touch/touch/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  touch
+//
+//  Created by Ariel Xin on 1/10/15.
+//  Copyright (c) 2015 cs48. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ touch/touch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  touch
+//
+//  Created by Ariel Xin on 1/10/15.
+//  Copyright (c) 2015 cs48. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
