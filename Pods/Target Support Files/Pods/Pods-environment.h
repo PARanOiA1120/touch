6c46295e67c42506943acc4513ebaca17da264c8
paranoia1120 <yuexin@umail.ucsb.edu>
2015-02-01 13:57:37 -0800

    half done with creating new event and posting new status

--- Pods/Target Support Files/Pods/Pods-environment.h
+++ Pods/Target Support Files/Pods/Pods-environment.h
@@ -12,6 +12,12 @@
 #define COCOAPODS_VERSION_MINOR_ActionSheetPicker_3_0 3
 #define COCOAPODS_VERSION_PATCH_ActionSheetPicker_3_0 11
 
+// FontAwesome+iOS
+#define COCOAPODS_POD_AVAILABLE_FontAwesome_iOS
+#define COCOAPODS_VERSION_MAJOR_FontAwesome_iOS 0
+#define COCOAPODS_VERSION_MINOR_FontAwesome_iOS 1
+#define COCOAPODS_VERSION_PATCH_FontAwesome_iOS 4
+
 // ICETutorial
 #define COCOAPODS_POD_AVAILABLE_ICETutorial
 #define COCOAPODS_VERSION_MAJOR_ICETutorial 1
@@ -23,6 +29,18 @@
 #define COCOAPODS_VERSION_MAJOR_IHKeyboardAvoiding 2
 #define COCOAPODS_VERSION_MINOR_IHKeyboardAvoiding 0
 #define COCOAPODS_VERSION_PATCH_IHKeyboardAvoiding 0
+
+// PPiAwesomeButton
+#define COCOAPODS_POD_AVAILABLE_PPiAwesomeButton
+#define COCOAPODS_VERSION_MAJOR_PPiAwesomeButton 1
+#define COCOAPODS_VERSION_MINOR_PPiAwesomeButton 5
+#define COCOAPODS_VERSION_PATCH_PPiAwesomeButton 2
+
+// PPiFlatSegmentedControl
+#define COCOAPODS_POD_AVAILABLE_PPiFlatSegmentedControl
+#define COCOAPODS_VERSION_MAJOR_PPiFlatSegmentedControl 1
+#define COCOAPODS_VERSION_MINOR_PPiFlatSegmentedControl 4
+#define COCOAPODS_VERSION_PATCH_PPiFlatSegmentedControl 0
 
 // RMDateSelectionViewController
 #define COCOAPODS_POD_AVAILABLE_RMDateSelectionViewController
