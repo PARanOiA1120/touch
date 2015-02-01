60568d36671ca8ab584b851f54c3c6aca16c051c
paranoia1120 <yuexin@umail.ucsb.edu>
2015-02-01 00:43:22 -0800

    post new status(not done yet)

--- touch/JCKit/UIButton+Button.h
+++ touch/JCKit/UIButton+Button.h
@@ -1,13 +1,13 @@
 //
-//  UIButton+JCButton.h
-//  jiechu
+//  UIButton+Button.h
+//  touch
 //
-//  Created by jianxd on 14/12/9.
-//  Copyright (c) 2014年 jiechu. All rights reserved.
+//  Created by Ariel Xin on 2/1/15.
+//  Copyright (c) 2015 cs48. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface UIButton (JCButton)
+@interface UIButton (Button)
 + (instancetype)eventTypeButtonWithFramg:(CGRect)frame normalImage:(UIImage *)nImage highlightImage:(UIImage *)hImage text:(NSString *)text;
 @end
--- touch/Model/NewsFeed/newsFeedManager.m
+++ touch/Model/NewsFeed/newsFeedManager.m
@@ -7,7 +7,55 @@
 //
 
 #import "newsFeedManager.h"
+#import "newsFeed.h"
+#import <AssetsLibrary/AssetsLibrary.h>
+#import "ProgressHUD.h"
+#import "Image.h"
+#import "Comment.h"
 
 @implementation newsFeedManager
++ (instancetype)sharedManager {
+    static newsFeedManager *sharedManager;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    return sharedManager;
+}
 
+- (void)createNewsFeed:(newsFeed *)newsFeed InBackgroundWithBlock:(PFBooleanResultBlock)block {
+    PFObject *object = [newsFeed PFObjectValue];
+    [object setObject:[User currentUser] forKey:NewsFeedCreator];
+    [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        if (!error) {
+            block(succeeded, error);
+        }
+    }];
+}
+
+
+- (void)getNewsFeedsInBackgroundwithParameters:(NSDictionary *)parameters WithBlock:(NewsFeedBlock)block
+{
+
+}
+
+//点赞
+- (void)likeNewsFeed:(NSString *)newsId ByUser:(PFUser *)user InBackgroundWithBlock:(PFBooleanResultBlock)block
+{
+    [ProgressHUD show:@"In progress" Interaction:NO];
+
+}
+
+
+- (void)unlikeNewsFeed:(NSString *)newsId ByUser:(User *)user InBackgroundWithBlock:(PFBooleanResultBlock)block
+{
+    [ProgressHUD show:@"In progress" Interaction:NO];
+    PFObject *object = [newsFeed getNewsFeedObject:newsId];
+    PFRelation *relation = [object relationforKey:NewsFeedLikeUsers];
+    [relation removeObject:[user getUserObject]];
+    [object saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        [ProgressHUD dismiss];
+        block(succeeded, error);
+    }];
+}
 @end
